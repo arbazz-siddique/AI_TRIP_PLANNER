fadbb6c945ecf19ec6e76ce51b79dfec2967c26a
arbazz-siddique <arbazzsiddiquecse@gmail.com>
2025-09-03 14:47:35 +0530

    implement userDetails in every page where needed

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,50 +1,61 @@
-import { Button } from '@/components/ui/button'
-import { SignInButton } from '@clerk/nextjs'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import { SignInButton, useUser } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-const menuOptions=[
-    {
-        name:"Home",
-        path:"/"
-    },
-    {
-        name:"Pricing",
-        path:"/pricing"
-    },
-    {
-        name:"Contact us",
-        path:"/contact-us"
-    },
-]
+const menuOptions = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Pricing",
+    path: "/pricing",
+  },
+  {
+    name: "Contact us",
+    path: "/contact-us",
+  },
+];
 
 const Header = () => {
+  const { user } = useUser();
   return (
-    <div className='flex justify-between items-center p-4'>
-        {/* log */}
-        <div className='flex gap-3 items-center'>
-        <Image src={'/logo.svg'} alt='logo' width={30} height={30} />
-        <h2 className='font-bold text-xl text-primary'>AI Trip Planner</h2>
-        </div>
-        {/* menu options */}
+    <div className="flex justify-between items-center p-4">
+      {/* log */}
+      <div className="flex gap-3 items-center">
+        <Image src={"/logo.svg"} alt="logo" width={30} height={30} />
+        <h2 className="font-bold text-xl text-primary">AI Trip Planner</h2>
+      </div>
+      {/* menu options */}
 
-        <div className='flex gap-8 items-center'>
-            {menuOptions.map((menu,index)=>(
-                <Link key={index} href={menu.path} className='text-lg hover:scale-105 transition-all hover:text-primary'>
-  <h2>{menu.name}</h2>
-</Link>
-              
-            ))}
-        </div>
+      <div className="flex gap-8 items-center">
+        {menuOptions.map((menu, index) => (
+          <Link
+            key={index}
+            href={menu.path}
+            className="text-lg hover:scale-105 transition-all hover:text-primary"
+          >
+            <h2>{menu.name}</h2>
+          </Link>
+        ))}
+      </div>
 
-        {/* Get started Button */}
-        <SignInButton mode='modal'>
-<Button>Get Started</Button>
+      {/* Get started Button */}
+      {!user ? (
+        <SignInButton mode="modal">
+          <Button>Get Started</Button>
         </SignInButton>
+      ) : (
+        <Link href={'/create-trip'}>
+        <Button>Create New Trip</Button>
+        </Link>
         
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- app/provider.tsx
+++ app/provider.tsx
@@ -1,40 +1,48 @@
-'use client'
-import React, { useEffect } from 'react'
-import Header from './_components/Header';
-import { useMutation } from 'convex/react';
-import { api } from '@/convex/_generated/api';
-import { useUser } from '@clerk/nextjs';
+"use client";
+import React, { useContext, useEffect, useState } from "react";
+import Header from "./_components/Header";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useUser } from "@clerk/nextjs";
+import { UserDetailContext } from "@/context/UserDetailContext";
 
 const Provider = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  
-  const CreateUser = useMutation(api.user.CreateNewUser)
-  const {user} = useUser();
+  const CreateUser = useMutation(api.user.CreateNewUser);
+  const [userDetail, setUserDetail] = useState<any>()
+  const { user } = useUser();
 
-  useEffect(()=>{
+
+  useEffect(() => {
     user && CreateNewUser();
-  },[user])
+  }, [user]);
 
-  const CreateNewUser = async () =>{
-    if(user){
-    // save new user if not exist
-    const result = await CreateUser({
-      email: user?.primaryEmailAddress?.emailAddress ?? '',
-      imageUrl: user?.imageUrl,
-      name: user?.fullName ?? ''
-    })
-
-  }
-}
+  const CreateNewUser = async () => {
+    if (user) {
+      // save new user if not exist
+      const result = await CreateUser({
+        email: user?.primaryEmailAddress?.emailAddress ?? "",
+        imageUrl: user?.imageUrl,
+        name: user?.fullName ?? "",
+      });
+      setUserDetail(result)
+    }
+  };
   return (
-    <div>
-        <Header/>
+    <UserDetailContext.Provider value={{userDetail, setUserDetail}}>
+      <div>
+        <Header />
         {children}
-        </div>
-  )
-}
+      </div>
+    </UserDetailContext.Provider>
+  );
+};
 
-export default Provider
+export default Provider;
+
+export const useUserDetail = ()=>{
+  return useContext(UserDetailContext);
+}
